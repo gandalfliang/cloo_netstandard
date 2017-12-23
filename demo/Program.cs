ae528aa99739e354cb1beb5de430ada4cffb14d5
liangtaiqi <liangtaiqi@cvte.com>
2017-12-23 12:21:25 +0800

    add demo

---
+++ demo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Cloo;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("hello from opencl");
+            //获取所有平台
+            var platforms = ComputePlatform.Platforms;
+            foreach(var platform in platforms)
+            {
+                Console.WriteLine($"{platform.Name},{platform.Version}");
+                //获取该平台下的计算设备
+                var devices = platform.QueryDevices();
+                foreach(var device in devices)
+                {
+                    Console.WriteLine($"    Device:{device.Name}");
+                }
+            }
+        }
+    }
+}
