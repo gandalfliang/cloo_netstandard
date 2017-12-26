972caac6e289f1f4628da125ba7e33d4752a100d
梁泰琪 <liangtaiqi@cvte.com>
2017-12-26 20:17:37 +0800

    fix character error

--- demo/demos/matrix_mul.cs
+++ demo/demos/matrix_mul.cs
@@ -40,23 +40,19 @@ namespace demo
                 sum[i, j] += matrixA[i, k] * matrixA[k, j];
 
             var elapsed = sw.Elapsed;
-            Console.WriteLine($"耗时: {elapsed.TotalMilliseconds} ms\n");
+            Console.WriteLine($"using: {elapsed.TotalMilliseconds} ms\n");
         }
 
         public void OpenClMul()
         {
-            //选取设备
+            //选取锟借备
             var platform = ComputePlatform.Platforms.FirstOrDefault();
             var device = platform.Devices.FirstOrDefault();
-            //设置相关上下文
             var properties = new ComputeContextPropertyList(platform);
             var context = new ComputeContext(new[] {device}, properties, null, IntPtr.Zero);
-            //命令队列，用于控制执行的代码
             ComputeCommandQueue commands = new ComputeCommandQueue(context, context.Devices[0],
                 ComputeCommandQueueFlags.None);
-            //读取opencl代码
             var code = File.ReadAllText(@"demos\cls\matrix_mul.cl");
-            //编译
             var program = new ComputeProgram(context, code);
             try
             {
@@ -66,27 +62,21 @@ namespace demo
             {
                 throw;
             }
-            //创建核心代码，就是cl代码中以kernel标识，函数签名为MatrixMul的函数
             var kernel = program.CreateKernel("MatrixMul");
-            //矩阵规模
             int rank = Rank;
-            //储存计算结果的数组
             var result = new ComputeBuffer<int>(context, ComputeMemoryFlags.WriteOnly, rank * rank);
             var matrix = CreateMatrix(context, rank);
-            //创建的核心代码函数以这种方式来传参
             kernel.SetMemoryArgument(0, result);
             kernel.SetMemoryArgument(1, matrix);
             kernel.SetValueArgument(2, rank);
-            Console.WriteLine($"运行平台: {platform.Name}\n运行设备： {device.Name}\n矩阵规模: {rank}x{rank}");
+            Console.WriteLine($"Platform: {platform.Name}\n Device: {device.Name}\n Size: {rank}x{rank}");
             Stopwatch sw = Stopwatch.StartNew();
-            //执行代码
             commands.Execute(kernel, null, new long[] {rank, rank}, null, null);
             int[] resultArray = new int[rank * rank];
             var arrHandle = GCHandle.Alloc(resultArray, GCHandleType.Pinned);
-            //读取数据
             commands.Read(result, true, 0, rank * rank, arrHandle.AddrOfPinnedObject(), null);
             var elapsed = sw.Elapsed;
-            Console.WriteLine($"耗时: {elapsed.TotalMilliseconds} ms\n");
+            Console.WriteLine($"using: {elapsed.TotalMilliseconds} ms\n");
             arrHandle.Free();
             kernel.Dispose();
         }
